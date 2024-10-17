6d9d003396a6bc3f2d1acabb0f0dcc8da3646834
Santiago Afanador Galeano <afanadorgaleano@gmail.com>
2024-10-17 10:26:41 -0500

    Test MongoDB connection

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
+const User = require('./schemas/User');
 
 // Load environment variables
-require('dotenv').config();  
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB_URI)
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Error connecting to MongoDB:', err));
+  .then(async () => {
+    console.log('Connected to MongoDB')
+
+    const users = await User.find();
+    console.log("Found Users:", users);
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+
+  })
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,7 +38,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', indexRouter);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
